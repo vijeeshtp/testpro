1500db62ed8d36d6012404cb8b4bb0e7100d72b3
vijeeshtp <vijeesh.tp@expertzlab.com>
2017-09-14 11:11:31 +0530

    updated pom with devtools and added src

---
+++ src/main/java/com/test/testpro/TestproApplication.java
@@ -0,0 +1,12 @@
+package com.test.testpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestproApplication.class, args);
+	}
+}
